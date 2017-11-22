96c8491598c9031007527ca9207d1dee3fbf7c34
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2017-11-22 12:40:04 +0200

    I did manage to make a copy (in the computer) of the local realm database from the app and to configure it with a customized key. That was fun. What is left, is to make an URL for the app (to copy the data in the app’s data), to manage the key for future fetching, and to prepare the Model.

--- ViewController.swift
+++ ViewController.swift
@@ -18,69 +18,41 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        //print(Realm.Configuration.defaultConfiguration.fileURL!)
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
         
         
         displayAllObjects()
-        deleteObject(with: 1)
-        displayAllObjects()
         
-        //cards = realm.objects(Card.self)
-   
-        
-        
-//        print("\n The number of objects is ", cards.count)
-//        
-//        print("The third object is: ", cards[2].name)
-//    
-//        for object in cards
-//        {
-//            
-//            print("\n###Object: ", object.name, "\n" ,object.text)
-//            
-////            if i==0
-////            {
-////                try! self.realm.write()
-////                {
-////                    self.realm.delete(object)
-////                }
-////            }
-////            i += 1;
+//        var key = Data(count: 64)
+//        _ = key.withUnsafeMutableBytes
+//        { bytes in
+//                SecRandomCopyBytes(kSecRandomDefault, 64, bytes)
 //        }
         
+        var s = "hellohellohellohellohellohellohellohellohellohellohellohellohell"
+        var key = Data(count: 64)
+        key = s.data(using: String.Encoding.utf8)!
         
         
+        let onHDD = URL(string: "///Users/alexinntekt/Desktop/lop.realm")
         
-        //populateDefaultCategories()
+        print("\n The encryption key is: #\(key)#\n")
+        
+        do
+        {
+            try Realm().writeCopy(toFile: onHDD!, encryptionKey: key)
+        }
+            catch
+            {
+                
+            }
+        
+
         
     }
 
-    func populateDefaultCategories()
-    {
-        
-        if cards.count == 0
-        { // 1
-            print("\n Running Realm for the first time")
-            try! realm.write()
-            { // 2
-                
-                let defaultCategories = [""] // 3
-                let itsName = "first card!"
-                let itsText = "hello hello hello! this is a card!"
-                
-                
-                
-                for category in defaultCategories
-                { // 4
-                    let newCategory = Card()
-                    newCategory.name = category
-                    realm.add(newCategory)
-                }
-            }
-            
-            cards = realm.objects(Card) // 5
-        }
-    }
+  
 
 
 }
